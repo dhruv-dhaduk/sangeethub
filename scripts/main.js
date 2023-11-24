9985e37575d9568e81d1a3583e3d270502aeea21
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-11-24 22:36:47 +0530

    Removed the unnecessary commented code

--- scripts/main.js
+++ scripts/main.js
@@ -8,22 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-function fetch_sheet() {
-    // const sheetId = "1HyoSoJHb2ol1HVEvSvF5e1jl-KnTx-7NFScFoNM7sus";
-    // const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
-    // const sheetName = "sangeethub";
-    // const query = encodeURIComponent("Select *");
-    // const url = `${base}&sheet=${sheetName}&tq=${query}`;
-
-    // const response = await fetch(url);
-    // const text = await response.text();
-
-    // const json = JSON.parse(text.substring(text.indexOf('(') + 1, text.lastIndexOf(')')));
-    // const videoIDs = [];
-    // for (const row of json.table.rows) {
-    //     if (row.c[0].v !== "links")
-    //         videoIDs.push(row.c[0].v);
-    // }
+function get_videoIDs() {
 
     const videoIDs = [
         "TxidFkYHDfI",
@@ -46,7 +31,7 @@ function fetch_sheet() {
     return videoIDs;
 }
 
-let videoIDs = fetch_sheet();
+let videoIDs = get_videoIDs();
 
 let player;
 let playerLoaded = false;
