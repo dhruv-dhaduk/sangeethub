55386308bd2a1699f890e91976c387b51485702c
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-11-28 16:34:10 +0530

    Called the populate queue function when data is available

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#player-btn-queue").addEventListener("click", showMusicQueue);
     document.querySelector("#music-queue-close-btn").addEventListener("click", hideMusicQueue);
 
+    const populateMusicQueueITV = setInterval(() => {
+        if (fetchStatus === -1) {
+            clearInterval(populateMusicQueueITV);
+        }
+        else if (fetchStatus === 1) {
+            populateMusicQueue(musicData);
+            clearInterval(populateMusicQueueITV);
+        }
+    }, 500);
+
     document.querySelector("#player-btn-playpause").addEventListener("click", () => {
         const stat = player.getPlayerState();
         if (stat === YT.PlayerState.PLAYING || stat === YT.PlayerState.BUFFERING)
@@ -49,6 +59,8 @@ function populateMusicQueue(data) {
         const item = createMusicQueueItem(music.id, music.title);
         queue.append(item);
     }
+
+    queue.scrollTo(0, 0);
 }
 
 function createMusicQueueItem(id, title) {
