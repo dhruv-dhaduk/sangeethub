8295411a270035c1fecb975b9ac66401b629dcc4
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-12-28 22:45:04 +0530

    Used the fetchWorker in main.js to fetch sheet

--- scripts/v2/main.js
+++ scripts/v2/main.js
@@ -19,3 +19,26 @@ document.addEventListener("DOMContentLoaded", () => {
         document.body.classList.add("disable-scroll");
     });
 });
+
+let fetchStatus = 0;
+let musicData;
+
+let fetchWorker = new Worker("./scripts/v2/fetchWorker.js");
+fetchWorker.addEventListener("message", (msg) => {
+    if (msg.data.sheet) {
+        musicData = msg.data.sheet;
+        fetchStatus = 1;
+    }
+    else if (msg.data.error) {
+        fetchStatus = -1;
+        console.log(`Couldn't fetch data : ${error}`);
+        alert(`Couldn't fetch data : ${error}`);
+    }
+    else {
+        fetchStatus = -1;
+        console.log("Error while fetching data.");
+        alert("Error while fetching data.");
+    }
+});
+
+fetchWorker.postMessage({command: "sheet"});
