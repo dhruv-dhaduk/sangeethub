22838efe0419bbc7de76d6da99792f1b6b0ceeaa
Dhruv Dhaduk <dhadukd44@gmail.com>
2023-12-28 23:08:43 +0530

    Added javascript to use the fetched sheet data and populate main feed

--- scripts/v2/init.js
+++ scripts/v2/init.js
@@ -6,10 +6,36 @@ function initMainFeed() {
 
         document.body.classList.remove("disable-scroll");
     });
+
+    const loadITV = setInterval(() => {
+        if (fetchStatus === 1) {
+            loadMainFeedItems();
+            clearInterval(loadITV);
+        }
+        else if (fetchStatus === -1) {
+            clearInterval(loadITV);
+        }
+    }, 100);
 }
 
 function initFooter() {
     document.querySelectorAll("#main-footer button").forEach((btn) => {
         btn.addEventListener("click", (e) => { e.stopPropagation(); });
     });
+}
+
+function loadMainFeedItems() {
+    const mainfeed = document.querySelector("#mainfeed");
+    const template = document.querySelector("#mainfeed-item-template").content.querySelector(".feeditem");
+
+    mainfeed.innerHTML = "";
+
+    for (const music of musicData) {
+        const item = template.cloneNode(true);
+        item.dataset.id = music.id;
+        item.querySelector(".feeditem-thumbnail-img").src = `https://img.youtube.com/vi/${music.id}/default.jpg`;
+        item.querySelector(".feeditem-title").innerHTML = music.title;
+
+        mainfeed.append(item);
+    }
 }
